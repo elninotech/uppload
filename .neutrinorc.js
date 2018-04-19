d8e0877fde12f851165989ed83ca9bfcc102df38
Anand Chowdhary <anandchowdhary@gmail.com>
2018-04-19 17:30:11 +0200

    Add things

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,10 +1,35 @@
 module.exports = {
   use: [
     '@neutrinojs/airbnb-base',
+    'neutrino-middleware-sass',
+    ['@neutrinojs/style-loader', {
+      loaders: [
+        {
+          loader: 'sass-loader',
+          useId: 'sass'
+        }
+    ]}],
     [
       '@neutrinojs/library',
       {
-        name: 'uppload'
+        name: 'Uppload',
+        target: 'web',
+        libraryTarget: 'umd',
+        polyfills: {
+          async: true
+        },
+        babel: {
+          presets: [
+            ['babel-preset-env', {
+              targets: {
+                browsers: [
+                  'last 5 Chrome versions',
+                  'last 5 Firefox versions'
+                ]
+              }
+            }]
+          ]
+        }
       }
     ]
   ]
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('./lib.scss');
+
 const modal = document.createElement('div');
 modal.innerHTML = 'This is an appended element';
 document.body.appendChild(modal);
