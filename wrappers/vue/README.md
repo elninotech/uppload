a92dfd4a9f06e0cc29a1fad3c1fa8263ee5f7665
Anand Chowdhary <anandchowdhary@gmail.com>
2018-05-01 17:20:49 +0200

    Add wrapper for Vue

--- wrappers/vue/README.md
+++ wrappers/vue/README.md
@@ -1,1 +1,21 @@
 # Uppload Vue
+
+Uppload Vue is the official Vue.js component for [Uppload](https://github.com/elninotech/uppload), the open-source file uploading widget.
+
+## Installation
+
+```bash
+yarn add uppload-vue
+```
+
+```js
+import UpploadVue from "uppload-vue";
+```
+
+## Usage
+
+```html
+<uppload @success="onSuccess">
+    <button>Upload</button>
+</uppload>
+```
---
+++ wrappers/vue/uppload.vue
@@ -0,0 +1,38 @@
+<template>
+    <div @click.prevent="openUppload">
+        <slot />
+    </div>
+</template>
+
+<script>
+import Uppload from "uppload";
+export default {
+	props: {
+		settings: {
+			type: Object,
+			default: {}
+		}
+	},
+	data() {
+		return {
+			value: "",
+			uppload: null
+		};
+	},
+	mounted() {
+		this.uppload = new Uppload(this.settings);
+		this.uppload.on("fileUploaded", url => {
+			this.updateUrl(url);
+			this.$emit("success", url);
+		});
+	},
+	methods: {
+		openUppload() {
+			this.uppload.openModal();
+		},
+		updateUrl(url) {
+			console.log("UPDATE URL", url);
+		}
+	}
+};
+</script>
