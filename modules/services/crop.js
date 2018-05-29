9a0f408a8464335da9fa1649a0908c277bde868f
Anand Chowdhary <anandchowdhary@gmail.com>
2018-05-29 12:09:02 +0200

    Fixes #13

--- modules/services/crop.js
+++ modules/services/crop.js
@@ -78,7 +78,6 @@ export default scope => {
 							onInitialize: instance => {
 								const allImages = previewWindow.querySelectorAll("img");
 								for (let i = 0; i < allImages.length; i++) {
-									console.log(allImages[i]);
 									if (allImages[i].offsetHeight > previewWindow.offsetHeight) {
 										allImages[i].style.height = previewWindow.offsetHeight + "px";
 									}
