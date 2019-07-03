8433bf09eb7a180899a891daf4177721a07f6b5f
Anand Chowdhary <anandchowdhary@gmail.com>
2019-07-03 11:35:52 +0200

    Release v1.2.0

--- wrappers/angular/dist/esm2015/uppload-angular.js
+++ wrappers/angular/dist/esm2015/uppload-angular.js
@@ -3,7 +3,7 @@ import Uppload from 'uppload';
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes} checked by tsc
  */
 class UpploadEndpoint {
 }
@@ -32,7 +32,7 @@ UpploadEvents.uploadStarted = "uploadStarted";
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes} checked by tsc
  */
 class UpploadComponent {
     constructor() {
@@ -91,14 +91,15 @@ UpploadComponent.decorators = [
   `
             },] },
 ];
+/** @nocollapse */
 UpploadComponent.propDecorators = {
-    settings: [{ type: Input }],
-    event: [{ type: Output }]
+    "settings": [{ type: Input },],
+    "event": [{ type: Output },],
 };
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes} checked by tsc
  */
 class UpploadModule {
 }
@@ -111,12 +112,12 @@ UpploadModule.decorators = [
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes} checked by tsc
  */
 
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ * @suppress {checkTypes} checked by tsc
  */
 /**
  * Generated bundle index. Do not edit.
