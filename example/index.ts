f44cd935ed7e76376a0630e4915ad77a08dbe04f
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-30 14:50:46 -0700

    :sparkles: Support for return data function

--- example/index.ts
+++ example/index.ts
@@ -11,6 +11,6 @@ uppload.use(new Instagram());
 uppload.use(new Axios(() => ({
   method: "GET",
   url: "https://jsonplaceholder.typicode.com/todos/1"
-})));
+}), () => "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"));
 
 uppload.open();
--- packages/axios/index.ts
+++ packages/axios/index.ts
@@ -1,23 +1,31 @@
 import { UpploadUploader } from "../uploader";
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 type axiosFunction = (...params: any[]) => AxiosRequestConfig;
+type returnFunction = (response: AxiosResponse) => string;
+
 export default class Axios extends UpploadUploader {
   name = "fetch";
   endpoint?: string;
   configFunction?: axiosFunction;
+  getUrlFunction?: returnFunction;
+  getUrlFunctionKey?: string;
 
-  constructor(endpointDetails?: string | axiosFunction) {
+  constructor(endpointDetails?: string | axiosFunction, returnDetails?: string | returnFunction) {
     super();
     if (typeof endpointDetails === "string") {
       this.endpoint = endpointDetails;
     } else if (typeof endpointDetails === "function") {
       this.configFunction = endpointDetails;
     }
+    if (typeof returnDetails === "string") {
+      this.getUrlFunctionKey = returnDetails;
+    } else if (typeof returnDetails === "function") {
+      this.getUrlFunction = returnDetails;
+    }
   }
 
   upload = (file: Blob): Promise<string> => {
-    console.log("Uploading your image...", this.endpoint);
     return new Promise((resolve, reject) => {
       axios({
         method: "POST",
@@ -28,8 +36,13 @@ export default class Axios extends UpploadUploader {
         ...(this.configFunction ? this.configFunction(file) : undefined)
       })
         .then(response => {
-          console.log("GOT SUCCESS RESPONSE", response.data);
-          resolve(response.data.url);
+          if (typeof this.getUrlFunction === "function") {
+            return resolve(this.getUrlFunction(response));
+          } else if (this.getUrlFunctionKey) {
+            return resolve(response.data[this.getUrlFunctionKey]);
+          } else {
+            return resolve(response.data.url);
+          }
         })
         .catch(error => reject(error));
     });
