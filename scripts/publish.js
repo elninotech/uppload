f52741b254e321e5fc979719599ac3c6f363be35
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-30 16:29:08 -0700

    :sparkles: Build all packages

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,20 +1,30 @@
-const fs = require("fs-extra");
+const { readdir, copy, ensureDir } = require("fs-extra");
 const { join } = require("path");
 const { exec } = require("shelljs");
 
 const publish = async () => {
 
   const packagesDir = join(__dirname, "..", "packages");
-  const packagesList = await fs.readdir(packagesDir);
-  console.log(packagesList);
+  const packagesList = await readdir(packagesDir);
 
-  packagesList.forEach(package => {
-    console.log(`Building ${package}...`);
-    console.log(exec(`cd ../packages/${package} && npm install && npm run build${
-      process.env.NPM_TOKEN ? "&& npm publish --access public" : ""
-    }`).stdout);
-  });
-;
+  console.log("Building all packages...");
+  console.log(exec("cd .. && npm run build").stdout);
+
+  for await (const package of packagesList) {
+    console.log(`Getting package ready: ${package}`);
+    await ensureDir(join(__dirname, "..", "packages", package, "dist"));
+    await copy(
+      join(__dirname, "..", "dist", "packages", package),
+      join(__dirname, "..", "packages", package, "dist")
+    );
+  }
+
+  for await (const package of packagesList) {
+    if (process.env.NPM_TOKEN) {
+      console.log(`Publishing package: ${package}`);
+      console.log(exec(`cd ../packages/${package} && npm publish --access public && cd ../..`).stdout);
+    }
+  }
 }
 
 publish()
