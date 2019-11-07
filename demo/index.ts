155a5da7abf0c7a9e008334ab00a643267c49dc6
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-06 18:55:26 -0800

    :sparkles: Add i18n, interfaces

--- demo/index.ts
+++ demo/index.ts
@@ -1,18 +1,18 @@
-// import Uppload from "@uppload/core";
+import { Uppload } from "../src/uppload";
 // import Axios from "@uppload/axios";
 // import Instagram from "@uppload/instagram";
-// import { en } from "@uppload/i18n";
+import { en } from "../src/i18n";
 
-// const uppload = new Uppload({
-//   value: "https://via.placeholder.com/150",
-//   bind: ".uppload-image",
-//   call: ".uppload-button",
-//   lang: en
-// });
+const uppload = new Uppload({
+  value: "https://via.placeholder.com/150",
+  bind: ".uppload-image",
+  call: ".uppload-button",
+  lang: en
+});
 // uppload.use(new Instagram());
 // uppload.use(new Axios(() => ({
 //   method: "GET",
 //   url: "https://jsonplaceholder.typicode.com/todos/1"
 // }), () => "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"));
 
-// uppload.open();
+uppload.open();
---
+++ src/i18n/en.ts
@@ -0,0 +1,3 @@
+export default {
+  "response_not_ok": "We got an error in uploading this file"
+}
