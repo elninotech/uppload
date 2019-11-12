f7fa80da3248ea32ca31de2935a3913c6ec71e73
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-11 16:55:03 -0800

    :construction: Add basic cropping

--- src/effects/crop.ts
+++ src/effects/crop.ts
@@ -1,28 +1,52 @@
 import { UpploadEffect } from "../";
-import { safeListen } from "../helpers/elements";
+import Cropper from "cropperjs";
 
 export default class Crop extends UpploadEffect {
   name = "crop";
   icon = `<svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><path d="M74 0v182h134v-25h-26V74H99V48h96c4 0 7 1 9 4 3 2 4 5 4 9v121h48v26h-48v48h-26v-48H61a13 13 0 01-13-13V74H0V48h48V0h26z" fill="#000" fill-rule="nonzero"/></svg>`;
 
   template = (file: Blob) => {
+    const image = URL.createObjectURL(file);
     return `
-      <div>This is crop</div>
-      <div class="settings">
-        <input type="range">
-        <span class="value"><span>0</span>%</span>
+      <div class="uppload-cropping-element">
+        <img style="width: 20px" alt="" src="${image}">
       </div>
     `;
   };
 
   handlers = () => {
-    const range = document.querySelector(
-      ".settings input[type='range']"
-    ) as HTMLInputElement;
-    if (range)
-      safeListen(range, "change", () => {
-        this.update();
+    const cropperElement = document.querySelector(
+      ".uppload-cropping-element img"
+    ) as HTMLImageElement | null;
+    if (cropperElement) {
+      const parent = cropperElement.parentElement as HTMLElement;
+      const currentDimensions = cropperElement.getBoundingClientRect();
+      const dimensions = parent.getBoundingClientRect();
+      if (currentDimensions.height < currentDimensions.width) {
+        cropperElement.style.height = `${dimensions.height}px`;
+        cropperElement.style.width = "auto";
+      } else {
+        cropperElement.style.width = `${dimensions.width}px`;
+        cropperElement.style.height = "auto";
+      }
+      requestAnimationFrame(() => {
+        const currentDimensions = cropperElement.getBoundingClientRect();
+        if (currentDimensions.height > dimensions.height) {
+          cropperElement.style.height = `${dimensions.height}px`;
+          cropperElement.style.width = "auto";
+        } else if (currentDimensions.width > dimensions.width) {
+          cropperElement.style.width = `${dimensions.width}px`;
+          cropperElement.style.height = "auto";
+        }
+        requestAnimationFrame(() => {
+          const cropper = new Cropper(cropperElement, {
+            crop(event) {
+              console.log("Got event", event);
+            }
+          });
+        });
       });
+    }
   };
 
   update() {
