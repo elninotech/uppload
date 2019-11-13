67757d7350b82462b06c92d97ad1750835c59d8e
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-13 13:23:46 -0800

    :wrench: Add test configuration file

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest"
+  }
+};
