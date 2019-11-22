2771362f282d15474e50d22beb38dfb922d6237a
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-21 19:02:45 -0800

    :lipstick: Add play icon to try button

--- demo/index.ts
+++ demo/index.ts
@@ -38,7 +38,7 @@ import {
 } from "../src";
 
 const button = document.createElement("div");
-button.innerHTML = `<div class="try-button"><button class="try-uppload">Try Uppload</button></div>`;
+button.innerHTML = `<div class="try-button"><button class="try-uppload"><span aria-hidden="true">&#9654;&nbsp;&nbsp;</span>Try Uppload</button></div>`;
 const header = document.querySelector("header#masthead .masthead");
 if (header) header.appendChild(button);
 
