bab85a88cd9210c81fe34d07df9767e13fd4745f
Anand Chowdhary <mail@anandchowdhary.com>
2019-12-19 15:01:28 -0500

    :wrench: Add external and globals

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,23 @@ const rollup = require("rollup");
 const rollupTypescript = require("@rollup/plugin-typescript");
 const exec = require("child_process").exec;
 
+const globals = {
+  "focus-trap": "createFocusTrap",
+  mitt: "mitt",
+  cropperjs: "Cropper"
+};
+
 gulp.task("build", async function () {
   const bundle = await rollup.rollup({
     input: "./src/index.ts",
     plugins: [
       rollupTypescript()
+    ],
+    external: [
+      "whatwg-fetch",
+      "focus-trap",
+      "mitt",
+      "cropperjs"
     ]
   });
 
@@ -17,18 +29,21 @@ gulp.task("build", async function () {
     file: "./dist/index.umd.js",
     format: "umd",
     name: "uppload",
-    sourcemap: true
+    sourcemap: true,
+    globals
   });
 
   await bundle.write({
     file: "./dist/index.cjs.js",
     format: "cjs",
-    sourcemap: true
+    sourcemap: true,
+    globals
   });
 
   await bundle.write({
     file: "./dist/index.amd.js",
     format: "amd",
-    sourcemap: true
+    sourcemap: true,
+    globals
   });
 });
