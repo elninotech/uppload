415166a508e5e545ef5213b594566e6c5628570c
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 01:42:01 +0530

    :sparkles: Add build examples

--- scripts/build-examples.js
+++ scripts/build-examples.js
@@ -2,6 +2,13 @@ const download = require("download");
 const extract = require("extract-zip");
 const { exec } = require("child_process");
 const { join } = require("path");
+const {
+  readdirSync,
+  copyFileSync,
+  mkdirSync,
+  writeFileSync,
+  readFileSync
+} = require("fs");
 
 const examplesDir =
   "https://github.com/elninotech/uppload-examples/archive/master.zip";
@@ -14,4 +21,32 @@ const examplesDir =
     if (error) return console.error(error);
     console.log("Extracted examples zip");
   });
+  const examples = readdirSync(
+    join(__dirname, "uppload-examples-master", "examples")
+  );
+  mkdirSync(join(__dirname, "..", "content", "examples"), { recursive: true });
+  examples.forEach(example => {
+    copyFileSync(
+      join(
+        __dirname,
+        "uppload-examples-master",
+        "examples",
+        example,
+        "README.md"
+      ),
+      join(__dirname, "..", "content", "examples", `${example}.md`)
+    );
+    writeFileSync(
+      join(__dirname, "..", "content", "examples", `${example}.md`),
+      readFileSync(
+        join(__dirname, "..", "content", "examples", `${example}.md`)
+      )
+        .toString()
+        .replace(
+          "##",
+          `You can [**view the source code on GitHub**](https://github.com/elninotech/uppload-examples/tree/master/examples/uppload-examples) or [try it on CodeSandbox](https://codesandbox.io/s/github/elninotech/uppload-examples/tree/master/examples/uppload-examples).\n\n##`
+        )
+    );
+  });
+  console.log("Added examples!");
 })();
