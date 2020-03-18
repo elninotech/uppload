653dbfd004eefdd9c298cfef02fe3479f736ca5e
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 01:33:42 +0530

    :heavy_plus_sign: Add extract-zip

--- scripts/build-examples.js
+++ scripts/build-examples.js
@@ -1,11 +1,17 @@
 const download = require("download");
+const extract = require("extract-zip");
 const { exec } = require("child_process");
-// const extract = require("extract-zip");
+const { join } = require("path");
 
 const examplesDir =
   "https://github.com/elninotech/uppload-examples/archive/master.zip";
 
 (async () => {
   console.log("Downloading examples...");
-  await download(examplesDir, __dirname);
+  await download(examplesDir, __dirname, { filename: "master.zip" });
+  console.log("Downloaded zip");
+  extract(join(__dirname, "master.zip"), { dir: __dirname }, error => {
+    if (error) return console.error(error);
+    console.log("Extracted examples zip");
+  });
 })();
