653496232fa4a2d22cc4437aacc3512144ea21f6
Cristiano AP <c.numb2@gmail.com>
2022-07-24 17:41:13 -0300

    feat: added formatBytes function (#550)

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -5,3 +5,31 @@
  */
 export const minifyHTML = (html: string) =>
   html.replace(/\n/g, "").replace(/  /g, "");
+
+/**
+ * Returns a formatted version of the bytes
+ * @param bytes - The bytes to format
+ * @param decimals - The number of decimals to show
+ * @returns The formatted string
+ */
+export const formatBytes = (bytes: number, decimals: number = 2) => {
+  if (bytes === 0) {
+    return "0 Bytes";
+  }
+  const base: number = 1024;
+  const dm: number = decimals < 0 ? 0 : decimals;
+  const sizes: string[] = [
+    "Bytes",
+    "KB",
+    "MB",
+    "GB",
+    "TB",
+    "PB",
+    "EB",
+    "ZB",
+    "YB",
+  ];
+  const index: number = Math.floor(Math.log(bytes) / Math.log(base));
+  const size: string = sizes[index];
+  return parseFloat((bytes / Math.pow(base, index)).toFixed(dm)) + " " + size;
+};
